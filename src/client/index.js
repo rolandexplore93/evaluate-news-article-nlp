054fdad0483674b791917b5fad4380acb14c4e63
rolandexplore93 <roland2rule@gmail.com>
2022-02-20 10:50:00 +0100

    installed babel, plugins and set up the dist folder

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
 console.log(checkForName);
+console.log(handleSubmit);
 
 alert("I EXIST")
 console.log("CHANGE!!");
